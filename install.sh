1e12bfc26e4ba6eae66b65a41ea28fb54d4a2cef
Ash Entwisle <ash.entwisle@protonmail.com>
2023-07-25 12:15:05 +0000

    updated readme, added install script and docker compose filel
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

---
+++ install.sh
@@ -0,0 +1,37 @@
+# make directory for openbot
+echo "Making directory..."
+mkdir -p $(pwd)/openbot
+cd $(pwd)/openbot
+
+# pull docker image
+echo "Pulling docker image..."
+docker pull ghcr.io/ashentwisle/openbot:main
+
+# get config file
+echo "Downloading config file..."
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot/main/config.json > config.json
+
+# get env file
+echo "Fetching enviroment variables"
+echo "DISCORD_TOKEN=your_token" > .env
+echo "DISCORD_ID=your_bot_id" >> .env
+
+# get docker-compose file
+echo "Downloading docker-compose file..."
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot/main/docker-compose.yml > docker-compose.yml
+
+# get license file
+echo "Fetching license..."
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot/main/LICENSE > LICENSE
+
+# get readme file
+echo "Fetching readme..."
+curl -fsSL https://raw.githubusercontent.com/ash-entwisle/openbot/main/README.md > README.md
+
+echo "Done!"
+echo "To start the bot, first edit the .env file with your bot's token and id."
+echo "Then run 'docker compose up' in the openbot directory."
+echo "To stop the bot, run 'docker compose down' in the openbot directory."
+echo "For more information, see the README.md file."
+echo "Any issues, feel free to open an issue on the github page: https://github.com/ash-entwisle/openbot" 
+
