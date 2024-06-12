6e05ee705af6bd92270c00a805ab397384cf3a7b
Ash Entwisle <ash.entwisle@protonmail.com>
2024-06-12 13:56:26 +0100

    restructured repo, need to sort out linking everything together
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-import { Logger } from './libs/logger'
-const app = new Hono()
-
-Logger.info('Server started')
-
-app.get('/', (c) => c.text('Hono!'))
-
-export default app
